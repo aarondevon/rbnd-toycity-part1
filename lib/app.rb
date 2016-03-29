0f8e0551c701cb89ee76d1747a11184fde0137f4
Aaron Sawyer <me@aarondevon.com>
2016-03-29 15:13:59 -0500

    Calculates total revenue, stock and price. error on price

--- lib/app.rb
+++ lib/app.rb
@@ -72,15 +72,50 @@ puts "|_|                                       "
 # For each brand in the data set:
   # Print the name of the brand
   products_hash["items"].each do |brand|
-    puts brand["brand"] if brand["stock"].to_i > 0
-    puts "********************"
+      current_brand = brand["brand"]
+    puts "#{brand["brand"]} \n********************" if brand["stock"].to_i > 0
 
-     # Count and print the number of the brand's toys we stock
-     puts "Number of Products: #{brand["stock"]}"
+      # Count the number of the brand's toys we stock
+      # Calculate the average price of the brand's toys
+      stock = []
+      price = []
+      brand_sales = []
+      total_brand_stock = 0
+      average_brand_price = 0
+      total_brand_sales = 0
+      products_hash["items"].each do |same|
+        if current_brand == same["brand"] #&& same["stock"].to_i > 0
+          stock << same["stock"].to_i
+          price << same["full-price"].to_f
+          same["purchases"].each do |brand_price|
+            brand_sales << brand_price["price"].to_f
+          end
+        end
+      end
+      # Print the number of the brand's toys we stock
+      stock.each do |add|
+        total_brand_stock += add
+      end
 
-     # Calculate and print the average price of the brand's toys
-     puts "********************"
+      puts "Number of Products: #{total_brand_stock}" if brand["stock"].to_i > 0
+
+      # Print the average price of the brand's toys
+      price.each do |add|
+        average_brand_price += add
+      end
+      # Try and think of different way of getting the number 2
+
+
+      average_brand_price = average_brand_price / 2
+      puts "Average Product Price: $#{average_brand_price.round(2)}" if brand["stock"].to_i > 0
+
+     # Calculate and print the total revenue of all the brand's toy sales combined
+
+     brand_sales.each do |add|
+       total_brand_sales += add
+     end
+     puts "Total Sales: #{total_brand_sales.round(2)}" if brand["stock"].to_i > 0
+     
      puts
+
   end
-  # Calculate and print the average price of the brand's toys
-  # Calculate and print the total revenue of all the brand's toy sales combined
